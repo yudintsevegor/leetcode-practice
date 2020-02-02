0183f2fb76b276163d5bc65e1eab00adc5f4de4b
Egor <yudintsevegor@gmail.com>
2020-02-02 21:40:28 +0300

    zigzag - done, but not very rude

--- zigZagConvert/convert.go
+++ zigZagConvert/convert.go
@@ -1,5 +1,62 @@
 package zigZagConvert
 
-func convert(s string, numRows int) string {
+import (
+	"strings"
+)
 
+func convert(s string, numRows int) string {
+	if numRows < 2 {
+		return s
+	}
+
+	letters := strings.Split(s, "")
+
+	mapHelper := make(map[int][]string, numRows)
+	for i := range mapHelper {
+		mapHelper[i] = make([]string, 0, 1)
+	}
+
+	isDownWalk := true
+	nextRow := 0
+	for i, letter := range letters {
+		ind, newCurrentRow, newDownWalk := getInd(isDownWalk, i, nextRow, numRows)
+		// log.Print(ind)
+		mapHelper[ind] = append(mapHelper[ind], letter)
+
+		nextRow = newCurrentRow
+		isDownWalk = newDownWalk
+	}
+
+	out := ""
+	for i := 0; i < numRows; i++ {
+		out += strings.Join(mapHelper[i], "")
+	}
+	return out
+}
+
+func getInd(isDownWalk bool, currentInd, nextRow, rowsNumber int) (int, int, bool) {
+	rowsMinusOne := rowsNumber - 1
+	if currentInd < rowsMinusOne {
+		nextRow++
+		return currentInd, nextRow, isDownWalk
+	}
+
+	if currentInd%rowsMinusOne == 0 {
+		if isDownWalk {
+			nextRow--
+			return rowsNumber - 1, nextRow, !isDownWalk
+		}
+		nextRow++
+		return 0, nextRow, !isDownWalk
+	}
+
+	if isDownWalk {
+		nextRow++
+		return currentInd % rowsMinusOne, nextRow, isDownWalk
+	}
+
+	newInd := nextRow
+	nextRow--
+
+	return newInd, nextRow, isDownWalk
 }
--- zigZagConvert/convert_test.go
+++ zigZagConvert/convert_test.go
@@ -2,6 +2,43 @@ package zigZagConvert
 
 import "testing"
 
-func TestFunc(t *testing.T) {
+type Case struct {
+	InputString string
+	NumRows     int
+	Expected    string
+}
 
+func TestFunc(t *testing.T) {
+	for i, c := range getCases() {
+		converted := convert(c.InputString, c.NumRows)
+		if converted != c.Expected {
+			t.Fatalf("[%v] Rows: %v; Result: %v; Expected: %v", i, c.NumRows, converted, c.Expected)
+		}
+	}
+}
+
+func getCases() []Case {
+	return []Case{
+
+		{
+			InputString: "PAYPALISHIRING",
+			NumRows:     3,
+			Expected:    "PAHNAPLSIIGYIR",
+		},
+		{
+			InputString: "PAYPALISHIRING",
+			NumRows:     4,
+			Expected:    "PINALSIGYAHRPI",
+		},
+		{
+			InputString: "PAYPALISHIRING",
+			NumRows:     1,
+			Expected:    "PAYPALISHIRING",
+		},
+		{
+			InputString: "PAYPALISHIRING",
+			NumRows:     2,
+			Expected:    "PYAIHRNAPLSIIG",
+		},
+	}
 }
