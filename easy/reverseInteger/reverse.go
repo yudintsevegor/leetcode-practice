02fb3754984644f8d2cb69a2280fe2c75c9c04bf
Egor <yudintsevegor@gmail.com>
2020-02-04 23:06:04 +0300

    reverse integer

---
+++ easy/reverseInteger/reverse.go
@@ -0,0 +1,42 @@
+package reverseInteger
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+func reverse(x int) int {
+	edgePower := math.Pow(2, 31)
+	if edgePower < math.Abs(float64(x)) {
+		return 0
+
+	}
+
+	splited := strings.Split(strconv.Itoa(x), "")
+	if x < 0 {
+		splited = splited[1:]
+	}
+
+	length := len(splited) - 1
+	for i := 0; i <= (length)/2; i++ {
+		fromLeft := splited[i]
+		splited[i] = splited[length-i]
+		splited[length-i] = fromLeft
+	}
+
+	joined := strings.Join(splited, "")
+	res, _ := strconv.Atoi(joined)
+
+	if edgePower < math.Abs(float64(res)) {
+		return 0
+
+	}
+
+	if x < 0 {
+		res = -res
+	}
+
+	return res
+
+}
---
+++ easy/reverseInteger/reverse_test.go
@@ -0,0 +1,52 @@
+package reverseInteger
+
+import (
+	"testing"
+)
+
+type Case struct {
+	Input    int
+	Expected int
+}
+
+func TestFunc(t *testing.T) {
+	for i, c := range getCases() {
+		result := reverse(c.Input)
+		if result != c.Expected {
+			t.Fatalf("[%d] Expected: %v; Result: %v; Input: %v", i, c.Expected, result, c.Input)
+		}
+	}
+}
+
+func getCases() []Case {
+	return []Case{
+		{
+			Input:    123,
+			Expected: 321,
+		},
+		{
+			Input:    -123,
+			Expected: -321,
+		},
+		{
+			Input:    120,
+			Expected: 21,
+		},
+		{
+			Input:    100,
+			Expected: 1,
+		},
+		{
+			Input:    1,
+			Expected: 1,
+		},
+		{
+			Input:    10,
+			Expected: 1,
+		},
+		{
+			Input:    1534236469,
+			Expected: 0,
+		},
+	}
+}
