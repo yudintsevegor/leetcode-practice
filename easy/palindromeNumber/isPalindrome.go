f17ac8d0a0803b37d0f4e67d75641c5fa3f05249
yudintsevegor <yudintsevegor@gmail.com>
2020-02-09 19:51:56 +0300

    isPalindrome - done

--- easy/palindromeNumber/isPalindrome.go
+++ easy/palindromeNumber/isPalindrome.go
@@ -1,5 +1,44 @@
 package palindromeNumber
 
+var (
+	numbers            = make([]int, 0, 1)
+	multiplier         = 10
+	previousMultiplier = 1
+)
+
 func isPalindrome(x int) bool {
-	return false
+	if x < 0 {
+		return false
+	}
+
+	for {
+		if x/multiplier == 0 {
+			numbers = append(numbers, x/previousMultiplier)
+			break
+		}
+
+		numbers = append(numbers, (x%multiplier)/previousMultiplier)
+
+		multiplier *= 10
+		previousMultiplier *= 10
+	}
+
+	multiplier = 10
+	previousMultiplier = 1
+
+	length := len(numbers)
+	if length < 2 {
+		numbers = numbers[:0]
+		return true
+	}
+
+	for i := 0; i < length/2; i++ {
+		if numbers[i] != numbers[length-1-i] {
+			numbers = numbers[:0]
+			return false
+		}
+	}
+
+	numbers = numbers[:0]
+	return true
 }
