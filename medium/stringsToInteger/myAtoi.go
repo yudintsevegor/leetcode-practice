ace412940b00772fab11d34d296c026060f9d397
yudintsevegor <yudintsevegor@gmail.com>
2020-02-09 18:22:44 +0300

    new result

--- medium/stringsToInteger/myAtoi.go
+++ medium/stringsToInteger/myAtoi.go
@@ -6,60 +6,57 @@ import (
 	"strings"
 )
 
-
 const (
-	plusSign = "+"
+	plusSign  = "+"
 	minusSign = "-"
 )
 
-var edge = math.Pow(2,31)
+var allowedSymbols = map[string]struct{}{
+	plusSign:  {},
+	minusSign: {},
+	"0":       {},
+	"1":       {},
+	"2":       {},
+	"3":       {},
+	"4":       {},
+	"5":       {},
+	"6":       {},
+	"7":       {},
+	"8":       {},
+	"9":       {},
+}
+
+var edge = math.Pow(2, 31)
 
 func myAtoi(str string) int {
-	if len(str) == 0{
+	if len(str) == 0 {
 		return 0
 	}
 
 	splitted := strings.Split(strings.TrimSpace(str), "")
-	if len(splitted) == 0{
+	if len(splitted) == 0 {
 		return 0
 	}
 
-	var allowedSymbols = map[string]struct{}{
-		"+": {},
-		"-": {},
-		"0": {},
-		"1": {},
-		"2": {},
-		"3": {},
-		"4": {},
-		"5": {},
-		"6": {},
-		"7": {},
-		"8": {},
-		"9": {},
-
-	}
-
-	if _, ok := allowedSymbols[splitted[0]]; !ok{
+	if _, ok := allowedSymbols[splitted[0]]; !ok {
 		return 0
 	}
 
-
-	if splitted[0] != plusSign && splitted[0] != minusSign{
+	if splitted[0] != plusSign && splitted[0] != minusSign {
 		delete(allowedSymbols, plusSign)
 		delete(allowedSymbols, minusSign)
 	}
 
 	out := ""
 	counts := 0
-	for _, symbol := range splitted{
-		if _, ok := allowedSymbols[symbol]; !ok{
+	for _, symbol := range splitted {
+		if _, ok := allowedSymbols[symbol]; !ok {
 			break
 		}
 
-		if symbol == plusSign || symbol == minusSign{
+		if symbol == plusSign || symbol == minusSign {
 			counts++
-			if counts > 1{
+			if counts > 1 {
 				break
 			}
 		}
@@ -67,18 +64,20 @@ func myAtoi(str string) int {
 		out += symbol
 	}
 
-	res, err := strconv.ParseFloat(out,64)
-	if err != nil{
+	allowedSymbols[plusSign] = struct{}{}
+	allowedSymbols[minusSign] = struct{}{}
+
+	res, err := strconv.ParseFloat(out, 64)
+	if err != nil {
 		return 0
 	}
 
-
-	if math.Abs(res) >=  edge{
-		if res < 0{
+	if math.Abs(res) >= edge {
+		if res < 0 {
 			return -int(edge)
 		}
 
-		return int(edge-1)
+		return int(edge - 1)
 	}
 
 	return int(res)
